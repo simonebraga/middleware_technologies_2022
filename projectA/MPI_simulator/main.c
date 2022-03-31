c32f1251948224800ceb574f7410ed72af7aaf46
bertulli <10565357+bertulli@users.noreply.github.com>
2022-03-31 14:45:43 +0200

    Added check on missing parameters, changed variable storing
    Now the parameters are stored in a vector, indicized by a enum. Two
    functions perform the check: initialize_parameters sets all the
    parameters to 0 (treated as null value), missing_parameter performs
    the check. A simple if statement in the main function terminates the
    program if something is missing

--- projectA/MPI_simulator/main.c
+++ projectA/MPI_simulator/main.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N_PARAMETERS 11
+
+int missing_parameter(int parameters[]);
+void initialize_parameters(int parameters[]);
+
+enum parameter { P, V, W, L, Np, Nv, Dp, Dv, Vp, Vv, t };
+
 static int debugFlag = 0;
 
 int main(int argc, char *argv[]) {
@@ -11,7 +18,8 @@ int main(int argc, char *argv[]) {
 
   // simulation parameters
   // NOTE: THEY ARE SIMPLE INTS FOR NOW!
-  int P, V, W, L, Np, Nv, Dp, Dv, Vp, Vv, t;
+  //  int P, V, W, L, Np, Nv, Dp, Dv, Vp, Vv, t;
+  int parameters[N_PARAMETERS];
 
   static const struct option longOptions[] = {
       // debug option
@@ -50,6 +58,9 @@ int main(int argc, char *argv[]) {
   int ret_char; // it is an int and not a char for safety reasons
   int option_index = 0;
 
+  // initialize parameters vector
+  initialize_parameters(parameters);
+
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
@@ -58,74 +69,74 @@ int main(int argc, char *argv[]) {
                                  longOptions, &option_index)) != -1) {
     switch (ret_char) {
     case 0:
-      if(debugFlag && myRank == 0){
-	printf("Recognized option \"%s\"\n", longOptions[option_index].name);
+      if (debugFlag && myRank == 0) {
+        printf("Recognized option \"%s\"\n", longOptions[option_index].name);
       }
       break;
     case 'P':
-      P = atoi(optarg);
+      parameters[P] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Number of people: %d\n", P);
+        printf("Number of people: %d\n", parameters[P]);
       }
       break;
     case 'V':
-      V = atoi(optarg);
+      parameters[V] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Number of vehicles: %d\n", V);
+        printf("Number of vehicles: %d\n", parameters[V]);
       }
       break;
     case 'W':
-      W = atoi(optarg);
+      parameters[W] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Width of the region: %d m\n", W);
+        printf("Width of the region: %d m\n", parameters[W]);
       }
       break;
     case 'L':
-      L = atoi(optarg);
+      parameters[L] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Length of the region: %d m\n", L);
+        printf("Length of the region: %d m\n", parameters[L]);
       }
       break;
     case 'n':
-      Np = atoi(optarg);
+      parameters[Np] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Noise per person: %d dB\n", Np);
+        printf("Noise per person: %d dB\n", parameters[Np]);
       }
       break;
     case 'N':
-      Nv = atoi(optarg);
+      parameters[Nv] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Noise per vehicle: %d dB\n", Nv);
+        printf("Noise per vehicle: %d dB\n", parameters[Nv]);
       }
       break;
     case 'd':
-      Dp = atoi(optarg);
+      parameters[Dp] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Radius of a person: %d m\n", Dp);
+        printf("Radius of a person: %d m\n", parameters[Dp]);
       }
       break;
     case 'D':
-      Dv = atoi(optarg);
+      parameters[Dv] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Radius of a vehicle: %d m\n", Dv);
+        printf("Radius of a vehicle: %d m\n", parameters[Dv]);
       }
       break;
     case 'u':
-      Vp = atoi(optarg);
+      parameters[Vp] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Speed of a person: %d m/s\n", Vp);
+        printf("Speed of a person: %d m/s\n", parameters[Vp]);
       }
       break;
     case 'U':
-      Vv = atoi(optarg);
+      parameters[Vv] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Speed of a vehicle: %d m/s\n", Vv);
+        printf("Speed of a vehicle: %d m/s\n", parameters[Vv]);
       }
       break;
     case 't':
-      t = atoi(optarg);
+      parameters[t] = atoi(optarg);
       if (debugFlag && myRank == 0) {
-        printf("Time step: %d s\n", t);
+        printf("Time step: %d s\n", parameters[t]);
       }
       break;
     case '?':
@@ -133,7 +144,7 @@ int main(int argc, char *argv[]) {
         printf("Option \"%s\" caused an error.\n",
                longOptions[option_index].name);
         printf("opterr: %d\noptopt: %d\n", opterr, optopt);
-	printf("ret_char: %d (%c)\n", ret_char, ret_char);
+        printf("ret_char: %d (%c)\n", ret_char, ret_char);
       }
       break;
     default:
@@ -147,10 +158,46 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // check if all parameters have been specified
+  int missing_param;
+  if ((missing_param = missing_parameter(parameters))) {
+    if (myRank == 0) {
+      printf("Error! Parameter number %d is missing\n",
+             missing_param - 1); // see the function
+      printf("Exiting...\n");
+    }
+    MPI_Finalize();
+    return 1;
+  }
+
+  // if everithing is ok, we can continue
 
   printf("Hi, I'm process %d out of %d.\n", myRank, nProcesses);
 
   MPI_Finalize();
 
+  return 0;
+}
+
+/* To initialize the parameters vector to 0.
+   It must be called before parsing the command line options */
+
+void initialize_parameters(int parameters[]) {
+  for (int i = 0; i < N_PARAMETERS; i++) {
+    parameters[i] = 0;
+  }
+  return;
+}
+
+/* Checks the parameters have all been specified. If a missing parameter is
+   found, it is returned the index (as specified in the parameters enum. If
+   everything is fine, it returns 0 */
+
+int missing_parameter(int parameters[]) {
+  for (int i = 0; i < N_PARAMETERS; i++) {
+    if (parameters[i] == 0) {
+      return i + 1; // returning i doesn't signal error if i == 0
+    }
+  }
   return 0;
 }
