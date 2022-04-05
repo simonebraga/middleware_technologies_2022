474f34c22db5b5b558809842b8e46197bf1618ba
bertulli <10565357+bertulli@users.noreply.github.com>
2022-04-05 16:51:42 +0200

    Add first draft of MPI simulation implementation (only movement)
    Now the MPI executable actually simulates the movement of people and
    vehicles. The area of noise is not implemented, as well as the level
    of noise and the collection of data. Launching the single-thread.sh
    script provides a graphical simulation of the process

--- projectA/MPI_simulator/quickrun.sh
+++ projectA/MPI_simulator/quickrun.sh
@@ -2,4 +2,15 @@
 
 #Run the project with the maximum number of hardware threads
 
-mpirun --use-hwthread-cpus mpi_simulator
+mpirun --use-hwthread-cpus mpi_simulator \
+       -P 100 \
+       -V 10 \
+       -W 500 \
+       -L 300 \
+       --Np 3 \
+       --Nv 10 \
+       --Dp 2 \
+       --Dv 10 \
+       --Vp 1 \
+       --Vv 14 \
+       -t 1
