e0ceeb5813eb967cc2e56988da4f678a2d63a219
simonebraga <simone1.braga@mail.polimi.it>
2022-05-13 02:33:44 +0200

    Minor fixes of Spark queries

--- projectA/spark_backend/src/main/java/it/polimi/middleware/spark/utils/CustomFunctions.java
+++ projectA/spark_backend/src/main/java/it/polimi/middleware/spark/utils/CustomFunctions.java
@@ -1,4 +1,14 @@
 package it.polimi.middleware.spark.utils;
 
 public class CustomFunctions {
+
+    //TODO Rename this class as "PoiMap" and add a proper constructor
+
+    public static String computeNearestPoi(Double x, Double y) {
+
+        //TODO Implement the actual computation of the POI
+
+        return "computed_poi_id";
+    }
+
 }
