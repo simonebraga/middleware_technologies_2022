1e597f5cd8dea0fabbbf96592a640c0398281fbe
bertulli <10565357+bertulli@users.noreply.github.com>
2022-05-30 13:13:52 +0200

    [FIX] Add multiprocess coordination and leader net communication
    Now the processes spawned by MPI:
    + get their rand() functions initialized with their process ID (rank)
    as seed
    + call MPI_Reduce() after calculating their share of agents
    + don't attempt a connection if they're not the coordinator (root)
    process
    
    The coordinator (root) process:
    + opens the connection
    + perform an in-place reduction (see MPI_IN_PLACE)
    + formats and sends the data

--- projectA/MPI_simulator/single-thread.sh
+++ projectA/MPI_simulator/single-thread.sh
@@ -3,8 +3,8 @@
 ./mpi_simulator --db \
 		-P 20 \
 		-V 10 \
-		-W 10 \
-		-L 50 \
+		-W 20 \
+		-L 70 \
 		--Np 30 \
 		--Nv 70 \
 		--Dp 2 \
@@ -12,8 +12,10 @@
 		--Vp 1 \
 		--Vv 10 \
 		-t 10 \
-		--origin-latitude  45.4578090 \
-		--origin-longitude 9.1737100 \
+		--origin-latitude  0.0 \
+		--origin-longitude 0.0 \
 		--kafka-bridge-address \
-		# 25.29.114.127
 		127.0.0.1
+#		25.29.114.127
+		#45.4578090 \
+		     #9.1737100 \
