ae3d88859fc5de8e2c7fea9fa5b74a4f43077f87
Alessandro Bertulli <alessandro.bertulli96@gmail.com>
2022-06-06 12:09:33 +0200

    [akka_actors] Initial commit: pom.xml + .gitignore
    Set up .gitignore file for Java, Maven; IntelliJ and Eclipse.
    Set up pom.xml with Java version 11 and dependency for Akka e JUnit

---
+++ projectB/akka_actors/src/main/java/it/polimi/middlewareB/App.java
@@ -0,0 +1,13 @@
+package it.polimi.middlewareB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
