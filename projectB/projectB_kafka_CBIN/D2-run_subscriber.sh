dd254a274f8357f9116a40fa773160df736a1992
Alessandro Bertulli <alessandro.bertulli96@gmail.com>
2022-06-16 12:16:47 +0200

    [Kafka] Add first helper scripts + CLI Application + DebugConsumer
    + A new directory called projectB_kafka_CBIN is added. It contains
    helper scripts to correctly set up Kafka (assuming a single topic
    called "pending"). PLEASE, PLACE IT IN (OR REFERENCE IT FROM) YOUR
    KAFKA DIRECTORY!
    + Added a basic CLI application, with three options: generate dummy
    records, or exit (option 2, "Check completed tasks", is currently
    void)
    + Added a custom debug class, called DebugConsumer, that
    programmaticaly fetches the Kafka topic. Hopefully, it will be
    expanded into the Akka scheduler

---
+++ projectB/projectB_kafka_CBIN/D2-run_subscriber.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export KAFKA_BOOTSTRAP_IP=127.0.0.1
+export KAFKA_BOOTSTRAP_PORT=9092
+
+# if [ $# -eq 0 ]
+# then
+# 	echo "Usage: $0 topic_name"
+# 	echo ""
+# 	echo "The following topics are available:"
+# 	../bin/kafka-topics.sh --list --bootstrap-server $KAFKA_BOOTSTRAP_IP:$KAFKA_BOOTSTRAP_PORT
+# 	exit 1
+# fi
+
+../bin/kafka-console-consumer.sh --topic pending --from-beginning --bootstrap-server $KAFKA_BOOTSTRAP_IP:$KAFKA_BOOTSTRAP_PORT
