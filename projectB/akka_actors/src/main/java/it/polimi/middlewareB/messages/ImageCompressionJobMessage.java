0e38967db73b913c43ebe66f5cac4001638eb5e3
Alessandro Bertulli <alessandro.bertulli96@gmail.com>
2022-07-04 19:44:11 +0200

    [Akka][Kafka] Implement completedJobs topic publishing by actors
    Now JobSupervisorActors, upon receiving a JobCompletedMessage, publish
    the completion message on the Kafka completedJobs topic. To do so,
    actors and messages keep track of the task key.

--- projectB/akka_actors/src/main/java/it/polimi/middlewareB/messages/ImageCompressionJobMessage.java
+++ projectB/akka_actors/src/main/java/it/polimi/middlewareB/messages/ImageCompressionJobMessage.java
@@ -3,13 +3,16 @@ package it.polimi.middlewareB.messages;
 public class ImageCompressionJobMessage {
 
 
-    public ImageCompressionJobMessage(String inputFile, String outputFile, int compressionRatio, int duration) {
+    public ImageCompressionJobMessage(String key, String inputFile, String outputFile, int compressionRatio, int duration) {
+        this.key = key;
 		this.compressionRatio = compressionRatio;
 		this.inputFile = inputFile;
 		this.outputFile = outputFile;
         this.duration = duration;
 	}
 
+    public String getKey() { return key;
+    }
     public int getCompressionRatio() {
         return compressionRatio;
     }
@@ -25,6 +28,8 @@ public class ImageCompressionJobMessage {
     public int getDuration(){
         return duration;
     }
+
+    private final String key;
     private final int compressionRatio;
     private final String inputFile;
     private final String outputFile;
