c9740d3fd8f5e1901d3c528060d876c1bd95582a
bertulli <10565357+bertulli@users.noreply.github.com>
2022-07-11 16:03:50 +0200

    [MPI] Add sleep to not overflow the network buffers + param script
    Add a call to usleep() to allow proper network communication.
    Moreover, the scripts (*.sh) are now parametric in the address of the
    Kafka bridge

--- projectA/MPI_simulator/single-thread.sh
+++ projectA/MPI_simulator/single-thread.sh
@@ -11,11 +11,11 @@
 		--Dv 50 \
 		--Vp 1 \
 		--Vv 10 \
-		-t  \
+		-t 10 \
 		--origin-latitude 32000 \
 		--origin-longitude 2800 \
-		--kafka-bridge-address \
-		127.0.0.1
+		--kafka-bridge-address $1
 #		25.29.114.127
+#		127.0.0.1
 		#45.4578090 \
 		     #9.1737100 \
